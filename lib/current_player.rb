ece333e27c6841639b632f25ffef39b16785c8af
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-05-14 16:48:05 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,18 +8,16 @@ end
   turn_counter
 end
 
-#def current_player(board)
-#  turn = turn_count(board)
-#  if turn % 2 == 0
-#    return "X"
-  #else
-  #  return "O"
-  #end
-#end
+# def current_player(board)
+# turn = turn_count(board)
+#   if turn % 2 == 0
+#     return "X"
+#   else
+#    return "O"
+#   end
+# end
 
 def current_player(board)
   turn = turn_count(board)
-  turn % 2 ==0
-  ? "X"
-  : "O"
+  turn % 2 ==0 ? "X": "O"
 end
